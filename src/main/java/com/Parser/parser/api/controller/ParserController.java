f46c04aab42d29ebb59a3d18aa466cb313cb3ec2
EkLeRchEk <gulyja69@gmail.com>
2026-05-15 19:02:48 +0700

    Update ParserController.java

---
+++ src/main/java/com/Parser/parser/api/controller/ParserController.java
@@ -0,0 +1,31 @@
+```package com.Parser.parser.api.controller;
+
+import com.Parser.parser.application.service.Parser;
+import com.Parser.parser.infrastructure.CryptocurrencyEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/parser")
+@RequiredArgsConstructor
+public class ParserController {
+
+    private final Parser parser;
+
+    @PostMapping("/parse")
+    public List<CryptocurrencyEntity> parseAndSave() {
+        return parser.parse();
+    }
+
+    @GetMapping("/all")
+    public List<CryptocurrencyEntity> findAll() {
+        return parser.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<CryptocurrencyEntity> searchByName(@RequestParam String name) {
+        return parser.findByName(name);
+    }
+}```
